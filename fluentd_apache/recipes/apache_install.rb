9e32c526d88bc09cd66fd32f666d65c5c4f6142d
kappa <kappa@kappair-2.local>
2013-04-26 17:33:49 +0900

    add directory permission

--- fluentd_apache/recipes/apache_install.rb
+++ fluentd_apache/recipes/apache_install.rb
@@ -2,6 +2,10 @@ package "httpd" do
 	action :install
 end
 
+directory "/var/log/httpd" do
+	mode "00755"
+end
+
 service "httpd" do
 	supports :status => true, :restart => true, :reload => true
 	action [ :enable, :start ]
--- fluentd_apache/recipes/fluentd_install.rb
+++ fluentd_apache/recipes/fluentd_install.rb
@@ -3,9 +3,14 @@ package "td-agent" do
 end
 
 cookbook_file "/etc/td-agent/td-agent.conf" do
+	mode "00644"
 	source "td-agent.conf"
 	action :create
 end
+
+directory "/tmp/td-agent" do
+	mode "00777"
+endc
 
 service "td-agent" do
         supports :status => true, :restart => true, :reload => true
