a4493e2e7f50bbbce37d1ae457473adcc49f5b9e
kappa <kappa@kappair-2.local>
2013-04-29 04:10:54 +0900

    add stable_ruby_install

---
+++ stable_ruby_install/metadata.rb
@@ -0,0 +1,7 @@
+name             'stable_ruby_install'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures stable_ruby_install'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
