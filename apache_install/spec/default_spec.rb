26545ca233295e6b47e3aec3c2cca8a67bf9d869
kappa <kappa@kappair-2.local>
2013-05-01 08:47:43 +0900

    fixed

--- apache_install/spec/default_spec.rb
+++ apache_install/spec/default_spec.rb
@@ -3,19 +3,21 @@ require 'chefspec'
 describe 'apache_install::default' do
 	let (:chef_run) { ChefSpec::ChefRunner.new.converge 'apache_install::default' }
 	#
-	%w{httpd}.each do |package_name|
-		it 'installs #{package_name}' do
-			chef_run.should install_package package_name
+#	%w{httpd}.each do |package_name|
+		#it 'installs #{package_name}' do
+		it 'install httpd' do
+			#chef_run.should install_package package_name
+			expect(chef_run).to install_package 'httpd'
 		end
-	end
+#	end
 	#
 	it 'start httpd on boot' do
-		chef_run.should set_service_to_start_on_boot 'httpd'
+		expect(chef_run).to set_service_to_start_on_boot 'httpd'
 	end
 
 	describe 'change port' do
 		it 'create port.conf' do
-			chef_run.should create_cookbook_file '/etc/httpd/conf.d/port.conf'
+			expect(chef_run).to create_cookbook_file '/etc/httpd/conf.d/port.conf'
 			file = chef_run.cookbook_file('/etc/httpd/conf.d/port.conf')
 			expect(file).to be_owned_by('root','root')
 			expect(file.mode).to eq "00644"
