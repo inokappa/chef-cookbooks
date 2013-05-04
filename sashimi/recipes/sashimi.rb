8c65b1dd6ac1afb64d80cf76eaa2e11bc4498e10
kappa <kappa@kappair-2.local>
2013-05-05 07:15:38 +0900

    add sashimi

---
+++ sashimi/recipes/sashimi.rb
@@ -0,0 +1,9 @@
+sashimis = data_bag_item('fishies','sashimi')['menu']
+template  '/tmp/menu' do
+        owner "root"
+        group "root"
+        source "menu.erb"
+        variables(
+                :sashimis =>  sashimis
+        )
+end
