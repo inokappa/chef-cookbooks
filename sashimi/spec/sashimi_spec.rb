7e2604ea4d7d30cfa0d8362397b3f07359ca1096
kappa <kappa@kappair-2.local>
2013-05-08 08:50:19 +0900

    add file

--- sashimi/spec/sashimi_spec.rb
+++ sashimi/spec/sashimi_spec.rb
@@ -1,8 +1,13 @@
 require 'chefspec'
 
 describe 'sashimi::sashimi' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'sashimi::sashimi' }
-  it 'should do something' do
-    pending 'Your recipe examples go here.'
-  end
+	let (:chef_run) { ChefSpec::ChefRunner.new.converge 'sashimi::sashimi' }
+	it 'stub out data_bag_item' do
+		Chef::Recipe.any_instance.stub(:data_bag_item).and_return(Hash.new)
+	end
+	it 'should created /tmp/menu' do
+		expect (chef_run).to create_file '/tmp/menu'
+		file = chef_run.template ('')
+		expect (file).to be_owned_by ('','')
+	end
 end
